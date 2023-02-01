9a4f3dbc991c6be401227d9ed31287dd653b3e4b
DireChicken200 <35498545+DireChicken200@users.noreply.github.com>
2023-02-01 12:33:41 -0500

    Finished outline of NSF and Player classes

--- src/edu/wpi/group7/NSF.java
+++ src/edu/wpi/group7/NSF.java
@@ -1,5 +1,62 @@
 package edu.wpi.group7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class NSF
 {
+   protected String name;
+   protected List<Player> players;
+
+   public NSF(String name)
+   {
+      this.name = name;
+      this.players = new ArrayList<Player>();
+   }
+
+   public String getName()
+   {
+
+      return name;
+   }
+
+   public void setName(String name)
+   {
+
+      this.name = name;
+   }
+
+   public List<Player> getPlayers()
+   {
+
+      return players;
+   }
+
+   public void setPlayers(List<Player> players)
+   {
+
+      this.players = players;
+   }
+
+   public void listNSFPlayers()
+   {
+      String printable = this.toString() + "\'s players: ";
+      for(Player player : players)
+      {
+         printable += player.getName() + ",";
+      }
+      System.out.println(printable.substring(0, printable.length() - 2));
+   }
+
+   public void addPlayer(Player player)
+   {
+
+   }
+
+   @Override
+   public String toString()
+   {
+
+      return this.name;
+   }
 }
--- src/edu/wpi/group7/Player.java
+++ src/edu/wpi/group7/Player.java
@@ -1,5 +1,49 @@
 package edu.wpi.group7;
 
+import java.util.List;
+
 class Player
 {
+   private String name;
+   private List<NSF> NSFs;
+
+   public Player(String name)
+   {
+      this.name = name;
+   }
+
+   public String getName()
+   {
+
+      return name;
+   }
+
+   public void setName(String name)
+   {
+
+      this.name = name;
+   }
+
+   public List<NSF> getNSFs()
+   {
+
+      return NSFs;
+   }
+
+   public void setNSFs(List<NSF> NSFs)
+   {
+
+      this.NSFs = NSFs;
+   }
+
+   public void listPlayerNSFs()
+   {
+
+   }
+
+   public void addNSF(NSF nsf)
+   {
+
+   }
 }
+
